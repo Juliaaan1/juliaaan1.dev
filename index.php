d95b63afe8520e6c979f2c342d40446dc2291976
Ivan Abashin <juiceworkmail@gmail.com>
2022-11-30 21:41:13 +0300

    Перевел на русский, добавил несколько пунктов в список навыков.

--- index.php
+++ index.php
@@ -21,7 +21,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="icon" type="image/x-icon" href="/images/favicon.png">
-    <title>About me</title>
+
+    <title>Juliaaan1 | Обо мне</title>
 </head>
 <body>
 <div class="container">
@@ -30,19 +31,24 @@
             <img src="/images/art.jpg" class="img-fluid" alt="Hand drawn portrait">
         </div>
         <div class="col">
-            <h3>Ivan Abashin</h3>
-            <p>> <?= date_diff(new DateTime(), new DateTime('1996-09-25'))->format('%y years old'); ?></p>
-            <p>> Volgograd, Russian Federation</p>
-            <p>> Back-end developer since <b>May 2018</b></p>
-            <p>&nbsp;&nbsp;Already
-                <?= date_diff(new DateTime(), new DateTime('2018-05-04'))->format('%y years and %m months'); ?></p>
-            <p>> Team Lead</p>
+            <h3>Иван Абашин</h3>
+            <p>> <?= date_diff(new DateTime(), new DateTime('1996-09-25'))->format('Возраст: %y лет'); ?></p>
+            <p>> Волгоград, Россия</p>
+            <p>> Back-end разработчик с <b>мая 2018 года</b></p>
+            <p>&nbsp;&nbsp;Уже
+                <?= date_diff(new DateTime(), new DateTime('2018-05-04'))->format('%y лет и %m месяцев'); ?></p>
+            <p>> Тимлид, ментор, человек-баритон</p>
 
-            <h3>Skills</h3>
+            <h3>Навыки</h3>
             <p>> PHP 7.0 - 8.1</p>
-            <p>> Unit and integration testing</p>
+            <p>> Модульное и интеграционное тестирование</p>
+            <p>> Серверное администрирование (apache, nginx, php)</p>
+            <p>> OpenAPI 3</p>
+            <p>> Git, модели ветвления</p>
+            <p>> HTML, CSS (нормальная каскадность), JS (ES6)</p>
+            <p>> Twig</p>
 
-            <h3>Contacts</h3>
+            <h3>Контакты</h3>
             <p>> Email: <a href="mailto:juiceworkmail@gmail.com">juiceworkmail@gmail.com</a></p>
             <p>> Telegram: <a href="https://telegram.me/juliaaan1_original">@juliaaan1_original</a></p>
         </div>
@@ -81,6 +87,10 @@
     p {
         margin: 0;
         padding: 0;
+    }
+
+    .bottom-panel > p {
+        margin-bottom: 1rem;
     }
 
     h3 {
